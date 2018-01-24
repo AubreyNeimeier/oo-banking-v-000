8e6593945ef44024b973e163cbc2a1d58335661d
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-24 16:14:31 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,11 +27,18 @@ attr_accessor :sender, :receiver, :status, :amount
       sender.balance -= amount
       self.status = "complete"
     else
-      self.status = "rejected"
-     "Transaction rejected. Please check your account balance."
+      #self.status = "rejected"
+     #"Transaction rejected. Please check your account balance."
     end
 
   end
+
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+
+
 
   def reverse_transfer
     if self.status == "complete"
